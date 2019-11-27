1d8341a6e5319d291a08319c5613fd79a66f03a7
Joonas Suonperä <joonas.suonpera@gmail.com>
2019-11-27 12:48:45 +0200

    added login. works.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,71 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './Login.css';
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: "",
+            password: "",
+            usersArray: []
+        }
+
+        this.onLogin = this.onLogin.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/users')
+            .then(res => {
+                this.setState({
+                    usersArray: res.data
+                })
+            })
+            .catch((error) => {
+                console.log('Error: ' + error);
+            })
+    }
+
+    onLogin = (e) => {
+        e.preventDefault();
+        let message = document.getElementById("error-message-l");
+        let username = document.getElementById("username");
+        let password = document.getElementById("password");
+        const test = this.state.usersArray.find(user => user.username === this.state.username);
+        if(!test) {
+            username.value = "";
+            password.value = "";
+            message.innerHTML = "Wrong username or password";
+        } else {
+            if (this.state.password === test.password) {
+                window.location = '/home';
+            } else {
+                password.value = "";
+                username.value = "";
+                message.innerHTML = "Wrong username or password";
+            }
+        }
+        console.log(test);
+    }
+
+    onChangeUsername = (e) => {
+        e.preventDefault();
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangePassword = (e) => {
+        e.preventDefault();
+        this.setState({
+            password: e.target.value
+        });
+    }
+
     render() {
         return (
             <div>
@@ -12,13 +75,22 @@ export default class Login extends Component {
                             <img src="/logo.png" alt="" id="logo"/>
                         </div>
                         <div className="username-container fullwidth">
-                            <input id="username" type="text" placeholder="Username"/>
+                            <input
+                                id="username"
+                                type="text"
+                                placeholder="Username"
+                                onChange={this.onChangeUsername}/>
                         </div>
                         <div className="password-container">
-                            <input id="password" type="password" placeholder="Password"/>
+                            <input
+                                id="password"
+                                type="password"
+                                placeholder="Password"
+                                onChange={this.onChangePassword}/>
                         </div>
+                        <div id="error-message-l" className="grid-center"></div>
                         <div className="login-button-container fullwidth">
-                            <button id="login-button">
+                            <button id="login-button" onClick={this.onLogin}>
                                 LOGIN
                             </button>
                         </div>
