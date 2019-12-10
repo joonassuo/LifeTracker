fb24edfc25fa6f216648d97acf6edcf4f4135899
Joonas Suonpera <jsuonper@student.hive.fi>
2019-12-10 16:50:01 +0200

    not owkring, some minor changes made

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,53 +1,54 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+//import axios from "axios";
 import { Link } from "react-router-dom";
 import "./Frontpage.css";
-import Navbar from "./navbar.component.js";
+//import Navbar from "./navbar.component.js";
 import Menu from "./menu.component.js";
 import { useSelector } from "react-redux";
 
 const FrontPage = () => {
+  const [showHamburger, setHamburger] = useState(false);
+  const [userId, setUserId] = useState("");
+  const id = useSelector(state => state.userId);
 
-    const [showHamburger, setHamburger] = setState(false);
-    const [userId, setUserId] = setState("");
-    const id = useSelector(state => state.userId);
+  useEffect(() => {
+    console.log(showHamburger);
+    setUserId(id);
+  }, [id, showHamburger]);
 
-    useEffect(() => {
-        setUserId(id);
-    })
-
-
-  toggleHamburger = event => {
-    event.preventDefault();
-    this.setState({
-      showHamburger: !this.state.showHamburger
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <div className="app">
-          <div className="navbar">
-            <Navbar toggle={this.toggleHamburger} />
-          </div>
-          <div>
-            {this.state.showHamburger ? (
-              <div className="menu">
-                <Menu />
-              </div>
-            ) : null}
-          </div>
-          <div className="homescreen-grid">
-            <div className="welcome">WELCOME</div>
-            <div className="name">{this.state.username}</div>
-            <div className="add-summary">add summary</div>
-            <Link className="button-container" to="/summaries/add">
-              <img src="/add.png" className="add-btn" />
-            </Link>
+  return (
+    <div>
+      <div className="app">
+        <div className="navbar">
+          <div className="navbar-container">
+            <div className="hamburger-icon">
+              <img
+                src="/menu.png"
+                alt="hamburger"
+                onClick={() => setHamburger(true)}
+              />
+            </div>
+            <div className="logo">PLCHLDR</div>
           </div>
         </div>
+        <div>
+          {showHamburger} ? (
+          <div className="menu">
+            <Menu />
+          </div>
+          ) : null}
+        </div>
+        <div className="homescreen-grid">
+          <div className="welcome">WELCOME</div>
+          <div className="name">{userId}</div>
+          <div className="add-summary">add summary</div>
+          <Link className="button-container" to="/summaries/add">
+            <img src="/add.png" alt="plus-sign" className="add-btn" />
+          </Link>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default FrontPage;
