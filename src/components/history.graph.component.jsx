259be640316084cddd88530e7c0e0ea7e3e225ae
Joonas Suonpera <jsuonper@student.hive.fi>
2020-01-02 12:38:09 +0200

    graph not working at all

--- src/components/history.graph.component.jsx
+++ src/components/history.graph.component.jsx
@@ -1,7 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Line } from "react-chartjs-2";
+import axios from "axios";
+
+const data = {
+  labels: [],
+  datasets: [
+    {
+      data: [],
+      label: "Wake Up",
+      borderColor: "#3e95cd",
+      fill: false
+    },
+    {
+      data: [],
+      label: "Sleep",
+      borderColor: "#8e5ea2",
+      fill: false
+    },
+    {
+      data: [],
+      label: "Nicotine",
+      borderColor: "#3cba9f",
+      fill: false
+    },
+    {
+      data: [],
+      label: "Excercise",
+      borderColor: "#e8c3b9",
+      fill: false
+    },
+    {
+      data: [],
+      label: "Meditation",
+      borderColor: "#c45850",
+      fill: false
+    },
+    {
+      data: [],
+      label: "Mood",
+      borderColor: "#c45850",
+      fill: false
+    }
+  ]
+};
 
 const Graph = () => {
-  return <div>graph</div>;
+  const id = useSelector(state => state.userId);
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/summaries")
+      .then(res => {
+        res.data.map(e => {
+          if (e.userId === id) {
+            setUserData(userData.concat(e));
+          }
+        });
+      })
+      .catch(err => console.log(err));
+  }, [userData]);
+
+  const test = () => {
+    console.log("click");
+    console.log(userData);
+    data.datasets[0].data.push(4);
+  };
+
+  return (
+    <div>
+      <div className="chart">
+        <Line data={data} />
+      </div>
+      <button onClick={() => test()}>X</button>
+    </div>
+  );
 };
 
 export default Graph;
