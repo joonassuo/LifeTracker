289307071ab0ac600e25b454d5c5f904efef80b2
joonassuo <joonas.suonpera@gmail.com>
2020-02-12 21:20:09 +0200

    graph now works, next filter only user id summaries to graph and make it prettier

--- src/components/history.graph.component.jsx
+++ src/components/history.graph.component.jsx
@@ -3,79 +3,80 @@ import { useSelector } from "react-redux";
 import { Line } from "react-chartjs-2";
 import axios from "axios";
 
-const data = {
-  labels: [],
-  datasets: [
-    {
-      data: [],
-      label: "Wake Up",
-      borderColor: "#3e95cd",
-      fill: false
-    },
-    {
-      data: [],
-      label: "Sleep",
-      borderColor: "#8e5ea2",
-      fill: false
-    },
-    {
-      data: [],
-      label: "Nicotine",
-      borderColor: "#3cba9f",
-      fill: false
-    },
-    {
-      data: [],
-      label: "Excercise",
-      borderColor: "#e8c3b9",
-      fill: false
-    },
-    {
-      data: [],
-      label: "Meditation",
-      borderColor: "#c45850",
-      fill: false
-    },
-    {
-      data: [],
-      label: "Mood",
-      borderColor: "#c45850",
-      fill: false
-    }
-  ]
-};
-
 const Graph = () => {
-  const id = useSelector(state => state.userId);
-  const [userData, setUserData] = useState([]);
+	const id = useSelector(state => state.userId);
+	const [userData, setUserData] = useState([]);
+	const [data, setData] = useState({
+		labels: [],
+		datasets: [
+			{
+				data: [],
+				label: "Wake Up",
+				borderColor: "#3e95cd",
+				fill: false
+			},
+			{
+				data: [],
+				label: "Sleep",
+				borderColor: "#8e5ea2",
+				fill: false
+			},
+			{
+				data: [],
+				label: "Nicotine",
+				borderColor: "#3cba9f",
+				fill: false
+			},
+			{
+				data: [],
+				label: "Excercise",
+				borderColor: "#e8c3b9",
+				fill: false
+			},
+			{
+				data: [],
+				label: "Meditation",
+				borderColor: "#c45850",
+				fill: false
+			},
+			{
+				data: [],
+				label: "Mood",
+				borderColor: "#c45850",
+				fill: false
+			}
+		]
+	});
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/summaries")
-      .then(res => {
-        res.data.map(e => {
-          if (e.userId === id) {
-            setUserData(userData.concat(e));
-          }
-        });
-      })
-      .catch(err => console.log(err));
-  }, []);
+	useEffect(() => {
+		axios
+			.get("http://localhost:5000/summaries")
+			.then(res => {
+				setUserData(res.data);
+			})
+			.catch(err => console.log(err));
+	}, []);
 
-  const test = () => {
-    console.log("click");
-    console.log(userData);
-    data.datasets[0].data.push(4);
-  };
+	const test = () => {
+		userData.forEach(e => {
+			data.labels.push(e.date.substr(5, 5));
+			data.datasets[0].data.push(e.wake_up);
+			data.datasets[1].data.push(e.hit_the_sack);
+			data.datasets[2].data.push(e.nicotine);
+			data.datasets[3].data.push(e.excersice);
+			data.datasets[4].data.push(e.meditation);
+			data.datasets[5].data.push(e.mood);
+		});
+	};
 
-  return (
-    <div>
-      <div className="chart">
-        <Line data={data} />
-      </div>
-      <button onClick={() => test()}>X</button>
-    </div>
-  );
+	return (
+		<div>
+			<div className="chart">
+				<Line data={data} />
+			</div>
+			<button onClick={() => test()}>X</button>
+		</div>
+	);
 };
 
 export default Graph;
